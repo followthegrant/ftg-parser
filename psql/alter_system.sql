183bf5b2c7df7243ef42ec52bb73d80cf0683c11
Simon Wörpel <simon.woerpel@medienrevolte.de>
2021-12-08 14:02:14 +0100

    psql: add server configuration

---
+++ psql/alter_system.sql
@@ -0,0 +1,34 @@
+ALTER SYSTEM SET
+ max_connections = '1000';
+ALTER SYSTEM SET
+ shared_buffers = '46GB';
+ALTER SYSTEM SET
+ effective_cache_size = '138GB';
+ALTER SYSTEM SET
+ maintenance_work_mem = '2GB';
+ALTER SYSTEM SET
+ checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET
+ wal_buffers = '16MB';
+ALTER SYSTEM SET
+ default_statistics_target = '100';
+ALTER SYSTEM SET
+ random_page_cost = '1.1';
+ALTER SYSTEM SET
+ effective_io_concurrency = '200';
+ALTER SYSTEM SET
+ work_mem = '6029kB';
+ALTER SYSTEM SET
+ min_wal_size = '1GB';
+ALTER SYSTEM SET
+ max_wal_size = '16GB';
+ALTER SYSTEM SET
+ max_worker_processes = '48';
+ALTER SYSTEM SET
+ max_parallel_workers_per_gather = '4';
+ALTER SYSTEM SET
+ max_parallel_workers = '48';
+ALTER SYSTEM SET
+ max_parallel_maintenance_workers = '4';
+
+SELECT pg_reload_conf();
