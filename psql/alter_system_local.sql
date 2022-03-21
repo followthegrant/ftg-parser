a0a87d16b169ee92e137a533465129088c9dfa4b
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-03-21 22:50:13 +0100

    psql: tweak local performance settings

--- psql/alter_system_local.sql
+++ psql/alter_system_local.sql
@@ -1,27 +1,27 @@
 ALTER SYSTEM SET
- max_connections = '40';
+ max_connections = '100';
 ALTER SYSTEM SET
  shared_buffers = '4GB';
 ALTER SYSTEM SET
  effective_cache_size = '12GB';
 ALTER SYSTEM SET
- maintenance_work_mem = '2GB';
+ maintenance_work_mem = '1GB';
 ALTER SYSTEM SET
  checkpoint_completion_target = '0.9';
 ALTER SYSTEM SET
  wal_buffers = '16MB';
 ALTER SYSTEM SET
- default_statistics_target = '500';
+ default_statistics_target = '100';
 ALTER SYSTEM SET
  random_page_cost = '1.1';
 ALTER SYSTEM SET
  effective_io_concurrency = '200';
 ALTER SYSTEM SET
- work_mem = '13107kB';
+ work_mem = '5242kB';
 ALTER SYSTEM SET
- min_wal_size = '4GB';
+ min_wal_size = '1GB';
 ALTER SYSTEM SET
- max_wal_size = '16GB';
+ max_wal_size = '4GB';
 ALTER SYSTEM SET
  max_worker_processes = '8';
 ALTER SYSTEM SET
